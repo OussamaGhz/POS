d6c86319dd88c2d4cf30b9e4b1ce3acc4e4752c9
Oussama <sohaib.ghazi@outlook.fr>
2024-08-22 22:13:48 +0100

    first commit

---
+++ src/index.tsx
@@ -0,0 +1,12 @@
+import "./index.css"; // import css
+import App from "./app";
+
+import * as React from "react";
+import { createRoot } from "react-dom/client";
+
+const root = createRoot(document.getElementById("root") as HTMLElement);
+root.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>
+);

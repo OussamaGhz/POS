bf2ef54d1c745896365c0a2cbf5bdcab1b27dd88
Oussama <sohaib.ghazi@outlook.fr>
2024-08-27 14:25:49 +0100

    adding test button

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,10 +1,11 @@
 import React from 'react';
+import { Button } from '../components/ui/button';
 
 const HomePage = () => {
   return (
     <div className="flex flex-col md:flex-row h-screen w-full gap-4">
       <div className="order-2 md:order-1 w-full md:w-2/3 bg-gray-100 p-4 rounded-lg flex flex-col h-full">
-        <div className='h-2/3 '></div>
+        <div className='h-2/3 '><Button onClick={() => alert("hello")}>Click me</Button></div>
         <div className='h-1/3 '></div>
       </div>
 
